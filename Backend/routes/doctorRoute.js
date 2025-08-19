f89d06dcb8706b6b75a9bcb9d1f848033ae357af
rajneesh018-057 <rajneeshkumar3107@gmail.com>
2025-08-19 22:18:00 +0530

    Delete Backend directory

--- Backend/routes/doctorRoute.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { doctorlist,getDoctorById } from '../controllers/doctorController.js';
- 
-const doctorRouter = express.Router();
-doctorRouter.get('/list', doctorlist);
-doctorRouter.get('/:id', getDoctorById); 
-export default doctorRouter;
- 
